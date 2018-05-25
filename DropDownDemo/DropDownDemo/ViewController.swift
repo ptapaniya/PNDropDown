dce47f869bdb2f73b26a65a930245ee893f53d54
ptapaniya <37733737+ptapaniya@users.noreply.github.com>
2018-05-25 11:14:15 +0530

    Update ViewController.swift

--- DropDownDemo/DropDownDemo/ViewController.swift
+++ DropDownDemo/DropDownDemo/ViewController.swift
@@ -12,12 +12,12 @@ import PNDropDown
 class ViewController: UIViewController {
 
     @IBOutlet weak var label: UILabel!
-    var dropDown : UIDropDown!
+    var dropDown : PNDropDown!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        dropDown = UIDropDown(frame: CGRect(x: 0, y: 0, width: 300, height: 30))
+        dropDown = PNDropDown(frame: CGRect(x: 0, y: 0, width: 300, height: 30))
         dropDown.center = CGPoint(x: self.view.frame.midX, y: self.view.frame.midY - 200)
         dropDown.placeholder = "Select your country"
         dropDown.optionsArray = ["India", "USA", "China", "France", "Germany", "Italy", "Canada"]
